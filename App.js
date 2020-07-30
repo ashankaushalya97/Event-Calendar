e17d5c6a2d8ff5b1429672c37e9f943acedd93eb
ashankaushalya97 <ashankaushalya952@gmail.com>
2020-07-31 00:31:23 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,86 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import AgendaScreen from './screens/agenda';
+import Calendar from './screens/calendars';
+import CalendarList from './screens/calendarsList';
+import ExpCalendar from './screens/expandableCalendar';
+import HorzontalCal from './screens/horizontalCalendarList';
+import TimelineCal from './screens/timelineCalendar';
+
+
+const App: () => React$Node = () => {
+  return (
+    <>
+     <AgendaScreen/>
+    {/* <Calendar/> */}
+    {/* <CalendarList/> */}
+    {/* <ExpCalendar/>  */}
+    {/* <HorzontalCal/> */}
+    {/* <TimelineCal/> */}
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+});
+
+export default App;
